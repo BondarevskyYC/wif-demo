d8c748a8593443d3d3222b3a3c8613cc295f0f55
BondarevskyYC <bondarevsky@yandex-team.ru>
2025-08-17 21:34:14 +0300

    Update main.tf

--- cloud-terraform/main.tf
+++ cloud-terraform/main.tf
@@ -24,38 +24,20 @@ provider "yandex" {
   folder_id = var.folder_id
 }
 
-resource "yandex_serverless_container" "my_container2" {
-  name = "my-serverless-app-2026"
+//
+// Create a new Serverless Container.
+//
+resource "yandex_serverless_container" "test-container" {
+  name               = "some_name-mibon2025"
+  description        = "any description"
+  memory             = 256
+  execution_timeout  = "15s"
+  cores              = 1
+  core_fraction      = 100
+  service_account_id = var.sa_id
+  
   image {
     url = "cr.yandex/crprpai76362s82qgl49/bondarevsky:latest"
   }
-  cores           = 1
-  memory          = 256
-  concurrency     = 4
-  execution_timeout = "60s"
-  service_account_id = var.sa_id
-  environment = {
-    MY_ENV_VAR = "some_value"
-  }
 }
 
-resource "yandex_serverless_container" "my_container" {
-  name        = "my-bondarevsky-container-2026"
-  description = "A sample serverless container"
-  folder_id   = var.folder_id # Replace with your Yandex Cloud folder ID
-
-  # Define the revision for the container
-  revision {
-    image {
-      url     = "cr.yandex/crprpai76362s82qgl49/bondarevsky:latest" 
-    }
-
-    resources {
-      memory = 128 # Memory in MB
-      cores  = 1   # Number of cores
-    }
-
-    execution_timeout = "10s" # Timeout for container execution
-    service_account_id = var.sa_id # Replace with your service account ID
-  }
-}
